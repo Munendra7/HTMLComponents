7f78baab20ea5b05ded9bdec31043cc0681b98f7
Munendra <89851958+Munendra7@users.noreply.github.com>
2024-02-16 18:27:35 +0530

    Update and rename Concurrentapicalls.js to GetAllDatainPowerPages.js

--- GetAllDatainPowerPages.js
+++ GetAllDatainPowerPages.js
@@ -1,6 +1,3 @@
-To optimize the code further, we can utilize modern JavaScript features like async/await for asynchronous operations and array destructuring for cleaner code. Additionally, we can use `fetch` instead of `$.ajax` for making HTTP requests, as `fetch` is more modern and offers a simpler API. Here's the optimized version of the code:
-
-```javascript
 // Function to fetch data from dataverse with pagination
 async function fetchDataFromDataverse(api) {
     try {
@@ -37,12 +34,9 @@ async function fetchAllDataFromDataverse(api) {
 }
 
 // Event listener for button click
-$('#toggleControlBtn').on('click', async () => {
-    const apiEndpoint = '/_api/cr9f3_datatables?$select=cr9f3_column1,cr9f3_column2,cr9f3_column3,cr9f3_column4';
+$('#getData').on('click', async () => {
+    const apiEndpoint = '/_api/{tablename}';
     
     const allData = await fetchAllDataFromDataverse(apiEndpoint);
     console.log("FinalData", allData);
 });
-```
-
-In this optimized version, `fetch` is used for making HTTP requests, async/await is used for handling asynchronous operations, and array destructuring is employed for cleaner code. This should result in improved readability and performance.
